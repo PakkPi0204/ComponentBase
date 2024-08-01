751762ff7155177a75e2ae16cbbc5cdcf4871d21
PakkPitak <frankypakkawat@gmail.com>
2024-08-02 01:39:32 +0700

    15.4

---
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue'
+import CategoryCard from '@/components/CategoryCard.vue';
+import { ref, onMounted } from 'vue'
+import type { Event } from '@/types'
+import axios from 'axios';
+const events = ref<Event[] | null>(null)
+
+onMounted(() => {
+  axios
+  .get('https://my-json-server.typicode.com/PakkPi0204/CompoDatabase/events')
+  .then((response) => {
+    events.value = response.data
+  })
+  .catch((error) => {
+    console.error('There was an error!', error)
+  })
+})
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+  <!-- new element -->
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+  <div class="category">
+    <CategoryCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.category {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: right;
+}
+</style>
+
+
